a7c45ebc2295e51033e92a656be6efcc000dd895
Quan Trong Anh Huy <huy.quan@smoovapp.com>
2015-08-04 17:27:45 +0700

    Add category module

--- webshop-py/webshop/admin.py
+++ webshop-py/webshop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from webshop.models import Category
 
 # Create your admin models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('cat_name', 'parent_id', 'description')
+
+admin.site.register(Category, CategoryAdmin)
--- webshop-py/webshop/models.py
+++ webshop-py/webshop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    cat_name = models.CharField(max_length=255)
+    parent_id = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
+    description = models.CharField(max_length=255)
