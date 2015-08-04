8546c5c730220a02296571f2c8eee133b646c7f9
Quan Trong Anh Huy <huy.quan@smoovapp.com>
2015-08-04 18:05:56 +0700

    Fix category

--- webshop-py/webshop/admin.py
+++ webshop-py/webshop/admin.py
@@ -3,6 +3,10 @@ from webshop.models import Category
 
 # Create your admin models here.
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('cat_name', 'parent_id', 'description')
+    list_display = ('cat_name', 'parent_name', 'description')
+
+#class ApplicationAdmin(admin.ModelAdmin):
+#    list_display = ('text', 'applicant_name')
 
 admin.site.register(Category, CategoryAdmin)
+#admin.site.register(Application, ApplicationAdmin)
--- webshop-py/webshop/models.py
+++ webshop-py/webshop/models.py
@@ -1,9 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from django.db import models
 import uuid
 
 # Create your models here.
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    cat_name = models.CharField(max_length=255)
-    parent_id = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
-    description = models.CharField(max_length=255)
+    cat_name = models.CharField(max_length=255, verbose_name="Name")
+    parent_id = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL, verbose_name="Parent")
+    description = models.CharField(max_length=255, blank=True, verbose_name="Description")
+
+    def parent_name(self):
+        return self.parent_id.cat_name
+
+    parent_name.short_description = 'Parent Name'
+
+#class Application(models.Model):
+#    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#    applicant = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL, verbose_name="Parent")
+#    text = models.CharField(max_length=255, blank=True, verbose_name="text")
+
+#    def applicant_name(self):
+#        return self.applicant.name
+#    applicant_name.short_description = 'Applicant Name'
+
+#    def applicant_email(self):
+#        return self.applicant.email
+#    applicant_email.short_description = 'Applicant Email'
